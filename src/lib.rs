bef11fabc1773c1c4df62279d9ab03ca1aa4624f
Nihal Developer <nihaltmdev@gmail.com>
2023-06-10 23:41:32 +0530

    added swap and order triangle points fns

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::convert::TryInto;
 
+fn swap<T>(a: T, b: T) -> (T, T) {
+    (b, a)
+}
+
 pub fn init_pixels(width: u32, height: u32) -> Vec<u32> {
     let total_size : usize = (height*width).try_into().unwrap();
     let mut pixels : Vec<u32> = vec![0; total_size];
@@ -160,9 +164,6 @@ pub fn draw_circle(pixels: &mut Vec<u32>, width: u32, height: u32, x_center: u32
                 if j >= 0 && j<= height {
                     let y_diff = j as i32 - y_center_i32;
                     let y_diff_squared = y_diff * y_diff;
-//                    let distance_squared = x_diff_squared + y_diff_squared;
-//                    let inner_circle = (radius_i32 - thick as i32) * (radius_i32 - thick as i32);
-//                    let outer_circle = (radius_i32 + thick as i32) * (radius_i32 + thick as i32);
                     let distance = f64::sqrt((x_diff_squared + y_diff_squared) as f64);
                     if distance >= (radius_i32-thickness as i32) as f64 && distance <= radius_i32 as f64 {
                         pixels[(j*width + i) as usize] = color;
@@ -171,6 +172,81 @@ pub fn draw_circle(pixels: &mut Vec<u32>, width: u32, height: u32, x_center: u32
             }
         }
     }
+}
+
+// function does not work properly yet
+/*
+pub fn draw_sector(pixels: &mut Vec<u32>, width: u32, height: u32, x_center: u32, y_center: u32, radius: u32, start_angle: f32, end_angle: f32, color: u32) {
+    let angle_diff = end_angle - start_angle;
+    let angle_step = 0.01;
+    let x_center_f32 = x_center as f32;
+    let y_center_f32 = y_center as f32;
+    let radius_f32 = radius as f32;
+    let start_angle_rad = start_angle.to_radians();
+    let end_angle_rad = end_angle.to_radians();
+
+    for angle in (start_angle_rad..=end_angle_rad).step_by(angle_step as usize) {
+        let x = x_center_f32 + radius_f32 * angle.cos();
+        let y = y_center_f32 + radius_f32 * angle.sin();
+
+        let x1 = x.round() as u32;
+        let y1 = y.round() as u32;
+
+        if x1 >= 0 && x1 < width && y1 >= 0 && y1 < height {
+            pixels[(y1 * width + x1) as usize] = color;
+        }
+    }
+}
+*/
+
+fn sort_triangle_points(x1: u32, y1: u32, x2: u32, y2: u32, x3: u32, y3: u32) -> ((u32, u32), (u32, u32), (u32, u32)) {
+    if y1 > y2 {
+        let (x1, x2) = swap(x1, x2);
+        let (y1, y2) = swap(y1, y2);
+    }
+    if y2 > y3 {
+        let (x2, x3) = swap(x2, x3);
+        let (y2, y3) = swap(y2, y3);
+    }
+    if y1 > y2 {
+        let (x1, x2) = swap(x1, x2);
+        let (y1, y2) = swap(y1, y2);
+    }
+    ((x1, y1), (x2, y2), (x3, y3))
+}
+
+fn line_info(x1: u32, y1: u32, x2: u32, y2: u32, k: &mut u32, c: &mut u32) {
+    let (mut dy, mut dx) = (0,0);
+    if y1 > y2 {
+        dy = y1 - y2;
+    }
+    else {
+        dy = y2 - y1;
+    }
+    
+    if x1 > x2 {
+        dx = x1 - x2;
+    }
+    else {
+        dx = x2 - x1;
+    }
+
+    if dx != 0 {
+        *k = dy/dx;
+        *c = y1 - dy*x1/dx;
+    }
+}
+
+pub fn fill_triangle(pixels: &mut Vec<u32>, width: u32, height: u32, x1: u32, y1: u32, x2: u32, y2: u32, x3: u32, y3: u32, color: u32) {
+    let (p1, p2, p3) = sort_triangle_points(x1, y1, x2, y2, x3, y3);
+    let (x1, y1) = p1;
+    let (x2, y2) = p2;
+    let (x3, y3) = p3;
+
+    let (mut k12, mut c12, mut k23, mut c23, mut k13, mut c13) = (0,0,0,0,0,0);
+    line_info(x1, y1, x2, y2, &mut k12, &mut c12);
+    line_info(x2, y2, x3, y3, &mut k23, &mut c23);
+    line_info(x1, y1, x3, y3, &mut k13, &mut c13);
 }
 
 pub fn write_to_ppm(pixels: &Vec<u32>, width: u32, height: u32, file_path: &str) {
